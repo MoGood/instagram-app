e7bf22d303634627e80f7bb7994da71394e63d32
Nidhi <nithiarun@yahoo.com>
2019-07-09 11:04:31 -0700

    First commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,76 +1,79 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../../models/User');
-const gravatar = require('gravatar');
-const bcrypt = require('bcryptjs');
+// const User = require('../../models/User');
+// const gravatar = require('gravatar');
+// const bcrypt = require('bcryptjs');
 
 //  @route  POST api/users/register
 //  @desc   Register user
 //  @access Public
-router.post('/register', (req, res) => {
-  User.findOne({email: req.body.email })
-  .then(user => {
-    if (user) {
-      return res.status(400).json({
-        email: 'Email already exists'
-      })
-    } else {
-      const avatar = gravatar.url(req.body.email, {
-        s: '200',
-        r: 'pg',
-        d: 'mm'
-      });  
+router.get('/test', (req,res) => res.json({
+  msg : 'user works'
+}));
+// router.post('/register', (req, res) => {
+//   User.findOne({email: req.body.email })
+//   .then(user => {
+//     if (user) {
+//       return res.status(400).json({
+//         email: 'Email already exists'
+//       })
+//     } else {
+//       const avatar = gravatar.url(req.body.email, {
+//         s: '200',
+//         r: 'pg',
+//         d: 'mm'
+//       });  
 
-      const newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        avatar,
-        password: req.body.password
-      });
+//       const newUser = new User({
+//         name: req.body.name,
+//         email: req.body.email,
+//         avatar,
+//         password: req.body.password
+//       });
 
-      bcrypt.genSalt(10, (err, salt) => {
-        if (err) throw err;
-        bcrypt.hash(newUser.password, salt, 
-          (err, hash) => {
-           if (err) throw err;
-           newUser.password = hash;
-           newUser.save()
-            .then(user => res.json(user))
-            .catch(err => console.log(err)); 
-          });
-         });
-    }
-  })
-  .catch(err => console.log(err));
-})
+//       bcrypt.genSalt(10, (err, salt) => {
+//         if (err) throw err;
+//         bcrypt.hash(newUser.password, salt, 
+//           (err, hash) => {
+//            if (err) throw err;
+//            newUser.password = hash;
+//            newUser.save()
+//             .then(user => res.json(user))
+//             .catch(err => console.log(err)); 
+//           });
+//          });
+//     }
+//   })
+//   .catch(err => console.log(err));
+// })
 
-//  @route  POST api/users/login
-//  @desc   Login user
-//  @access Public
-router.post('/login', (req,res) => {
- const email = req.body.email;
- const password = req.body.password;
+// //  @route  POST api/users/login
+// //  @desc   Login user
+// //  @access Public
+// router.post('/login', (req,res) => {
+//  const email = req.body.email;
+//  const password = req.body.password;
  
- //Find user by email
- User.findOne({email})
-  .then(user => {
-   if (!user){
-    return res.status(404).json({
-      email: 'User not found'
-    }); 
-   }
+//  //Find user by email
+//  User.findOne({email})
+//   .then(user => {
+//    if (!user){
+//     return res.status(404).json({
+//       email: 'User not found'
+//     }); 
+//    }
    
-   //check password
-   bcrypt.compare(password, user.password)
-   .then(isMatch => {
-     if (isMatch) {
-       return res.json({msg: 'Sucess'}); 
-     }
-     return res.status(400).json(
-       {password: 'Password incorrect'});
-   })
-  })
-  .catch(err => console.log(err)); 
-})
+//    //check password
+//    bcrypt.compare(password, user.password)
+//    .then(isMatch => {
+//      if (isMatch) {
+//        return res.json({msg: 'Sucess'}); 
+//      }
+//      return res.status(400).json(
+//        {password: 'Password incorrect'});
+//    })
+//   })
+//   .catch(err => console.log(err)); 
+// })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -11,15 +11,15 @@ app.use(bodyParser.urlencoded({extended: false}
   ));
   app.use(bodyParser.json());
 
-//Db config
-// const db = require('./config/keys').mongoURI;
-// mongoose
-// .connect(db)
-// .then(()=> console.log('MongoDb connected'))
-// .catch(err => console.log(err));
+// Db config
+const db = require('./config/keys').mongoURI;
+mongoose
+.connect(db)
+.then(()=> console.log('MongoDb connected'))
+.catch(err => console.log(err));
 
 
-app.get('/', (req, res) => res.send('Hello'));
+app.get('/', (req, res) => res.send('Hello world'));
 
 app.use('/api/users', users);
 app.use('/api/profile', profile);
