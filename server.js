b4d4f8a1150d4b4e59e09a3eddd5323df9346205
Nidhi <nithiarun@yahoo.com>
2019-07-09 21:57:17 -0700

    jwt and passport
    Class 11 done

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 const bodyParser = require('body-parser');
+const passport = require ('passport');
 
 
 const app = express();
@@ -17,6 +18,13 @@ mongoose
 .connect(db)
 .then(()=> console.log('MongoDb connected'))
 .catch(err => console.log(err));
+
+//passport middleware
+app.use(passport.initialize());
+
+
+//passport configuaration 
+require('./config/passport')(passport);
 
 
 app.get('/', (req, res) => res.send('Hello world'));
