14bfc4c8ee6bff9408cff5b6c89cd010917c9c82
Seon Ricks <skcirs@gmail.com>
2019-07-12 18:52:43 -0700

    Post api and model

---
+++ validation/post.js
@@ -0,0 +1,18 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validatePostInput(data) {
+  let errors = {};
+
+  data.image = !isEmpty(data.image) ? data.image : '';
+
+
+  if (Validator.isEmpty(data.image)) {
+    errors.image = 'Image is required';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
