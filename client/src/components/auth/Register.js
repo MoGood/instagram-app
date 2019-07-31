1087b13271e16706ed3d26d19a31bcda229feceb
Seon Ricks <skcirs@gmail.com>
2019-07-30 18:25:45 -0700

    Update Register.js

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,109 +1,106 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {classnames} from 'classnames';
-import {registerUser} from '../../actions/authActions';
-import {connect} from 'react-redux';
-import { stat } from 'fs';
-
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { registerUser } from '../../actions/authActions';
+import TextFieldGroup from '../common/TextFieldGroup';
 
 class Register extends Component {
-  constructor(){
-      super();
-      this.state = {
-          handle:'',
-          email:'',
-          password:'',
-          password2:'',
-          errors:{}
-      };
-      this.onSubmit = this.onSubmit.bind(this);
+  constructor() {
+    super();
+    this.state = {
+      handle: '',
+      email: '',
+      password: '',
+      password2: '',
+      errors: {}
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
-    onChange(e)
-    {
-        this.setState({[e.target.handle]:e.target.value});
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
     }
-    onSubmit(e){
-        e.preventDefault();
-        const newUser ={
-            handle: this.state.handle,
-            email: this.state.email,
-            password: this.state.password,
-            password2: this.state.password2
-        };
+  }
 
-        this.props.registerUser(newUser, this.props.history);
-
-        
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
     }
-   componentWillReceiveProps(nextProps){
-     if(nextProps.errors){
-       this.setState({errors: nextProps.errors});
-     }
-   }
+  }
 
-    render() {
-      const {user} = this.props.auth;
-        const {errors} = this.state;
-        return (
-            <div className="register">
-              {user? user.name : null}
-            <div className="container">
-        <div className="row">
-          <div className="col-md-8 m-auto">
-            <h1 className="display-4 text-center">Sign Up</h1>
-            <p className="lead text-center">Create your Instagram account</p>
-            <form onSubmit={this.onSubmit} noValidate>
-              <div className="form-group">
-                <input type="text" className={classnames('form-control form-control-lg',{'is-invalid':errors.handle})}placeholder="Username" name="handle" 
-                value={this.state.handle}
-                onChange={this.onChange}
-                required />
-                {errors.handle && (
-                    <div className="invalid-feedback">
-                    {errors.handle}
-                    </div>
-                )}
-              </div>
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
-              <div className="form-group">
-                <input type="email" className={classnames('form-control form-control-lg',{'is-invalid':errors.email})} placeholder="Email Address" name="email" value={this.state.email}
-                onChange={this.onChange} />
-                <small className="form-text text-muted">email </small>
-                {errors.email && (
-                  <div className="invalid-feedback">
-                    {errors.email}
-                  </div>
-                )}
-              </div>
+  onSubmit(e) {
+    e.preventDefault();
 
-              <div className="form-group">
-                <input type="password" className={classnames('form-control form-control-lg',{'is-invalid':errors.password})} placeholder="Password" name="password" value={this.state.password}
-                onChange={this.onChange} />
-                {errors.password && (
-                  <div className="invalid-feedback">
-                    {errors.password}
-                  </div>
-                )}
+    const newUser = {
+      handle: this.state.handle,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    };
 
-              </div>
-              <div className="form-group">
-                <input type="password" className={classnames('form-control form-control-lg',{'is-invalid':errors.password2})} placeholder="Confirm Password" name="password2" value={this.state.password2}
-                onChange={this.onChange} />
-                {errors.password2 && (
-                  <div className="invalid-feedback">
-                    {errors.password2}
-                  </div>
-                )}
-              </div>
-              
-              <input type="submit" className="btn btn-info btn-block mt-4" />
-            </form>
+    this.props.registerUser(newUser, this.props.history);
+  }
+
+  render() {
+    const { errors } = this.state;
+
+    return (
+      <div className="register">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Sign Up</h1>
+              <p className="lead text-center">
+                Create your Instagram account
+              </p>
+              <form noValidate onSubmit={this.onSubmit}>
+                <TextFieldGroup
+                  placeholder="Username"
+                  name="handle"
+                  value={this.state.handle}
+                  onChange={this.onChange}
+                  error={errors.handle}
+                />
+                <TextFieldGroup
+                  placeholder="Email"
+                  name="email"
+                  type="email"
+                  value={this.state.email}
+                  onChange={this.onChange}
+                  error={errors.email}
+                  info="This site uses Gravatar so if you want a profile image, use a Gravatar email"
+                />
+                <TextFieldGroup
+                  placeholder="Password"
+                  name="password"
+                  type="password"
+                  value={this.state.password}
+                  onChange={this.onChange}
+                  error={errors.password}
+                />
+                <TextFieldGroup
+                  placeholder="Confirm Password"
+                  name="password2"
+                  type="password"
+                  value={this.state.password2}
+                  onChange={this.onChange}
+                  error={errors.password2}
+                />
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    )
+    );
   }
 }
 
@@ -111,12 +108,11 @@ Register.propTypes = {
   registerUser: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   errors: PropTypes.object.isRequired
-}
+};
 
-const mapStateToProps =(state) => ({
+const mapStateToProps = state => ({
   auth: state.auth,
   errors: state.errors
 });
 
-
-export default connect(mapStateToProps, {registerUser})(Register);
+export default connect(mapStateToProps, { registerUser })(withRouter(Register));
