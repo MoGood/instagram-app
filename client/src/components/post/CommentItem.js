1b36037e302daeced68c25bb5a6af05325b3f5a0
Seon Ricks <skcirs@gmail.com>
2019-08-02 17:52:24 -0700

    Link to profile of commenter

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { deleteComment } from '../../actions/postActions';
+import { Link } from 'react-router-dom';
 
 class CommentItem extends Component {
   onDeleteClick(postId, commentId) {
@@ -14,28 +15,28 @@ class CommentItem extends Component {
     return (
       <div className="card card-body mb-3">
         <div className="row">
-          <div className="col-md-2">
-            <a href="profile.html">
+          <div className="col-md-2 text-center comment-handle">
+            <Link to={`/profile/${comment.handle}`}>
               <img
                 className="rounded-circle d-none d-md-block"
                 src={comment.avatar}
                 alt=""
               />
-            </a>
+              {comment.handle}
+            </Link>
             <br />
-            <p className="text-center">{comment.name}</p>
           </div>
           <div className="col-md-10">
-            <p className="lead">{comment.text}</p>
             {comment.user === auth.user.id ? (
               <button
                 onClick={this.onDeleteClick.bind(this, postId, comment._id)}
                 type="button"
-                className="btn btn-danger mr-1"
+                className="btn btn-danger mr-1 float-right"
               >
                 <i className="fas fa-times" />
               </button>
             ) : null}
+            <p className="lead">{comment.text}</p>
           </div>
         </div>
       </div>
