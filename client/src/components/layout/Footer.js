01de5786a6854b3df09fded1fb65b5dd1f729cdc
Seon Ricks <skcirs@gmail.com>
2019-08-02 17:51:30 -0700

    Replace margin with padding on footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default () => {
   return (
-    <footer className="mt-5 p-4 text-center">
+    <footer className="p-4 text-center">
     &copy; {new Date().getFullYear()} INSTAGRAM
     </footer>
   )
