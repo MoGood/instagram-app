3b05bf12d94df85764e65f0565676d89be8e39ec
Seon Ricks <skcirs@gmail.com>
2019-08-02 17:49:43 -0700

    Link to profile from post

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -31,18 +31,18 @@ class PostItem extends Component {
     const { post, auth, showActions } = this.props;
 
     return (
-      <div className="card card-body mb-3">
+      <div className="card card-body mb-3 post-item">
         <div className="row">
           <div className="col-md-2">
-            <Link to="/profile">
+            <Link to={`/profile/${post.handle}`}>
               <img
                 className="rounded-circle d-none d-md-block"
                 src={post.avatar}
                 alt=""
               />
+              <br />
+              <p className="text-center comment-handle">{post.handle}</p>
             </Link>
-            <br />
-            <p className="text-center">{post.handle}</p>
           </div>
           <div className="col-md-10">
             <img src={post.image} alt="" />
@@ -55,7 +55,7 @@ class PostItem extends Component {
                   className="btn btn-light mr-1"
                 >
                   <i
-                    className={classnames('fas fa-thumbs-up', {
+                    className={classnames('text-secondary fas fa-thumbs-up', {
                       'text-info': this.findUserLike(post.likes)
                     })}
                   />
@@ -69,13 +69,13 @@ class PostItem extends Component {
                   <i className="text-secondary fas fa-thumbs-down" />
                 </button>
                 <Link to={`/post/${post._id}`} className="btn btn-info mr-1">
-                  Comments
+                  View or Add Comments
                 </Link>
                 {post.user === auth.user.id ? (
                   <button
                     onClick={this.onDeleteClick.bind(this, post._id)}
                     type="button"
-                    className="btn btn-danger mr-1"
+                    className="btn btn-danger mr-1 float-right"
                   >
                     <i className="fas fa-times" />
                   </button>
